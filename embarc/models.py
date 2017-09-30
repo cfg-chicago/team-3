9407e9142213832dce38bd687bd6ee149022e19f
charluo <charluo@umich.edu>
2017-09-29 21:14:02 -0400

    new models

--- embarc/models.py
+++ embarc/models.py
@@ -7,8 +7,21 @@ class Journey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(3000), unique=False, nullable=False)
-    picture = db.Column(db.String(200), unique=False, nullable=True)
+    picture = db.Column(db.String(250), unique=False, nullable=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-# class Reflection(db.Model):
+class Reflection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    content = db.Column(db.String(3000), unique=False, nullable=False)
+    journey = db.Column(db.String(200), unique=False, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    fb_login = db.Column(db.String(120), unique=False, nullable=False)
+    picture = db.Column(db.String(250), unique=False, nullable=True)
+
+
 
