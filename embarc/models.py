4fea9ada417769c2be96ea1399bd2ae0f6713345
charluo <charluo@umich.edu>
2017-09-29 20:53:43 -0400

    journey db

--- embarc/models.py
+++ embarc/models.py
@@ -5,3 +5,6 @@ from .app import db
 class Journey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(3000), unique=False, nullable=False)
+    picture = db.Column(db.String(200), unique=False, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
