7ee8f6d1180598f45d05ce1a9a5c7628556b7477
evanyeh <eyeh2809@yahoo.com>
2017-09-30 00:57:21 -0500

    add username* to session

--- embarc/views.py
+++ embarc/views.py
@@ -110,7 +110,7 @@ def login():
 
         if not user:
             user = User(username, password, user_type)
-            session['user_type'] = user_type
+            session['username'] = username
             db.session.add(user)
             db.session.commit()
         login_user(user)
