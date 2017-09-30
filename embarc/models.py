cce354d4c2dd4ef479e6ce018f8ce3e747639132
Jeremy Lua <jang@WSUSOM-09987.local>
2017-09-29 23:59:37 -0500

    changed models reflection to include journeyid

--- embarc/models.py
+++ embarc/models.py
@@ -14,7 +14,7 @@ class Reflection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(3000), unique=False, nullable=False)
-    journey = db.Column(db.String(200), unique=False, nullable=False)
+    journeyid = db.Column(db.Integer, unique=False, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class User(db.Model):
@@ -22,6 +22,3 @@ class User(db.Model):
     name = db.Column(db.String(120), unique=False, nullable=False)
     fb_login = db.Column(db.String(120), unique=False, nullable=False)
     picture = db.Column(db.String(250), unique=False, nullable=True)
-
-
-
