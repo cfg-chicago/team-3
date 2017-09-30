49fff19b6356ab0e41751723b4987180aab5d553
charluo <charluo@umich.edu>
2017-09-30 02:13:18 -0400

    time working again

--- embarc/models.py
+++ embarc/models.py
@@ -8,14 +8,14 @@ class Journey(db.Model):
     name = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(3000), unique=False, nullable=False)
     picture = db.Column(db.String(250), unique=False, nullable=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.date)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class Reflection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(3000), unique=False, nullable=False)
     journeyid = db.Column(db.Integer, unique=False, nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.date)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
