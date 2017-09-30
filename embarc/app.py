b77e0d9638861f268a90b5c91d1b935f9728102e
David Elliott <delliott@caltech.edu>
2017-09-29 17:16:54 -0700

    Add flask_sqlalchemy to requirements

--- embarc/app.py
+++ embarc/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 import redis
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
 socketio = SocketIO(app)
+
+db = SQLAlchemy(app)
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
